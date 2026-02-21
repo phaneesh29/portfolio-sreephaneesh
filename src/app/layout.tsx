851936dab8427995970368db713a77401c1626a9
phaneesh29 <kanugovisreephaneesha@gmail.com>
2026-02-21 08:37:12 +0530

    feat: Integrate Vercel Analytics for enhanced tracking and insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import Navbar from "@/components/Navbar";
 import StarCanvas from "@/components/StarCanvas";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <StarCanvas />
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
